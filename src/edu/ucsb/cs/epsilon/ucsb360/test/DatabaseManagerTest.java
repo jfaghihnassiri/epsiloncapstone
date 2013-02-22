62042f33712f88bbe92df64174c57e4fbf9b716b
Maxwell Hinson <maxwellhinson@gmail.com>
2013-02-22 15:37:24 -0800

    Created JUnit framework for DatabaseManager

---
+++ src/edu/ucsb/cs/epsilon/ucsb360/test/DatabaseManagerTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package edu.ucsb.cs.epsilon.ucsb360.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Max Hinson
+ *
+ */
+public class DatabaseManagerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#initUserData(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testInitUserData() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#initAugmentationData(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testInitAugmentationData() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#incrementViews(java.lang.String)}.
+	 */
+	@Test
+	public void testIncrementViews() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#incrementAugmentationsSeen(java.lang.String)}.
+	 */
+	@Test
+	public void testIncrementAugmentationsSeen() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#incrementAugmentationsShared(java.lang.String)}.
+	 */
+	@Test
+	public void testIncrementAugmentationsShared() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#incrementAugmentationsCreated(java.lang.String)}.
+	 */
+	@Test
+	public void testIncrementAugmentationsCreated() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.DatabaseManager#getBitmap(java.lang.String)}.
+	 */
+	@Test
+	public void testGetBitmap() {
+		fail("Not yet implemented");
+	}
+
+}
