ef5a475d97bcf4110791cba60794aa7b80562df4
andy168chipz <andy168chipz@gmail.com>
2013-02-25 17:57:14 -0800

    Added Java Doc style comments

--- src/edu/ucsb/cs/epsilon/ucsb360/MainActivity.java
+++ src/edu/ucsb/cs/epsilon/ucsb360/MainActivity.java
@@ -17,11 +17,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * MainActivity for UCSB 360
+ * Handles Facebook Log in and Log out, view without log in
+ * @author Andy Chou
+ */
 public class MainActivity extends Activity implements OnClickListener {
 
 	/*
 	 * Privates
 	 */
+	private int DEBUG = 1;
 	private Button authButton;
 	private Button btnViewNoLogin;
     boolean pendingRequest;
@@ -30,7 +36,12 @@ public class MainActivity extends Activity implements OnClickListener {
     static final String PENDING_REQUEST_BUNDLE_KEY = "com.facebook.samples.graphapi:PendingRequest";
     Session session;
     
-    
+    /**
+     * Populates after the splash screen
+     * @author Andy Chou
+     * @param saved instance of the previous state
+     * @return void
+     */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -48,6 +59,11 @@ public class MainActivity extends Activity implements OnClickListener {
         Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
 	}
 	
+	/**
+	 * Creates a new session used for Facebook 
+	 * @author Andy Chou
+	 * @return A newly activated session
+	 */
     private Session createSession() {
         Session activeSession = Session.getActiveSession();
         if (activeSession == null || activeSession.getState().isClosed()) {
@@ -57,6 +73,14 @@ public class MainActivity extends Activity implements OnClickListener {
         return activeSession;
     }
 	
+    /**
+     * Called after login operation is complete
+     * @author Andy Chou
+     * @param requestCode The requestCode parameter from the forwarded call. When this onActivityResult occurs as part of Facebook authorization flow, this value is the activityCode passed to open or authorize.
+     * @param resultCode An int containing the resultCode parameter from the forwarded call.
+     * @param data The Intent passed as the data parameter from the forwarded call.
+     * 
+     */
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
     	if (this.session.onActivityResult(this, requestCode, resultCode, data) &&
                 pendingRequest &&
@@ -65,47 +89,75 @@ public class MainActivity extends Activity implements OnClickListener {
             .onActivityResult(this, requestCode, resultCode, data);
     		Intent i = new Intent(this, CameraView.class);
 			startActivity(i);
-			Log.d(TAG, "onActivityResult OPEN");
         }
-
-
     }
+    /**
+     * (COPIED FROM GOOGLE ANDROID PAGE)
+     * This method is called after onStart() when the activity is being re-initialized from a previously saved state, given here in savedInstanceState.
+     *  Most implementations will simply use onCreate(Bundle) to restore their state, but it is sometimes convenient to do it here after all of the initialization has been done or to allow subclasses to decide whether to use your default implementation. 
+     *	The default implementation of this method performs a restore of any view state that had previously been frozen by onSaveInstanceState(Bundle).
+     *
+     *
+     *	THIS SHOULD NEVER BE CALLED AS OF 2/24/13
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
+        if(DEBUG == 1)
+        {
+        	Log.d(TAG,"RESTOR INSTANCE");
+        }
         pendingRequest = savedInstanceState.getBoolean(PENDING_REQUEST_BUNDLE_KEY, pendingRequest);
     }
+    /**
+     * (COPIED FROM GOOGLE ANDROID PAGE)
+     * Called to retrieve per-instance state from an activity before being killed so that the state can be restored in onCreate(Bundle) or onRestoreInstanceState(Bundle)
+     *  (the Bundle populated by this method will be passed to both).
+	 *	This method is called before an activity may be killed so that when it comes back some time in the future it can restore its state. 
+	 * For example, if activity B is launched in front of activity A, and at some point activity A is killed to reclaim resources, 
+	 *	activity A will have a chance to save the current state of its user interface via this method so that when the user returns to activity A, 
+	 *	the state of the user interface can be restored via onCreate(Bundle) or onRestoreInstanceState(Bundle).
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
+        if(DEBUG == 1)
+        {
+        	Log.d(TAG,"SAVEINSTANCE");
+        }
         outState.putBoolean(PENDING_REQUEST_BUNDLE_KEY, pendingRequest);
     }
-    
+
+    /**
+     * Handles button Clicking
+     * @author Andy Chou
+     * @param the current view
+     * 
+     */
     public void onClick(View v) {
     	switch(v.getId())
     	{
+    		//button for no log in view
     		case R.id.btnViewNoLogin : 
     		{
     			Intent i = new Intent(this, CameraView.class);
     			startActivity(i);
     			break;
     		}
+    		//button for FB log in
     		case R.id.authButton:
     		{
     			if(this.session.isOpened())
     			{
-    				Log.d(TAG,"ISOPEN TRUE");
+    					//close session, then create a new one to init state
     				this.session.closeAndClearTokenInformation();
-    				pendingRequest = false;
-    				Log.d(TAG,"should be false");
     				this.session = createSession();
     			}
     			else
     			{
     			StatusCallback callback = new StatusCallback() {
                     public void call(Session session, SessionState state, Exception exception) {
+                    	//called when user quit FB log in, 
                         if (exception != null) {
                             new AlertDialog.Builder(MainActivity.this)
                                     .setTitle(R.string.login_failed_dialog_title)
@@ -118,7 +170,9 @@ public class MainActivity extends Activity implements OnClickListener {
                     }
                     
                 };
+                
                 pendingRequest = true;
+                // Used for FB Log in
                 this.session.openForRead(new Session.OpenRequest(this).setCallback(callback));
     			
     			}
