1d5b96ed758f4d2ec2fb0cf2baa1c1fa9f8d5426
Maxwell Hinson <maxwellhinson@gmail.com>
2013-03-01 23:26:40 -0800

    Created stubs for UserTest, FriendTest, and FriendManagerTest classes.

---
+++ src/edu/ucsb/cs/epsilon/ucsb360/test/FriendManagerTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package edu.ucsb.cs.epsilon.ucsb360.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Max Hinson
+ *
+ */
+public class FriendManagerTest {
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.FriendManager#addFriend(java.lang.String)}.
+	 */
+	@Test
+	public void testAddFriend() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.FriendManager#getNumFriends()}.
+	 */
+	@Test
+	public void testGetNumFriends() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.FriendManager#getName(java.lang.String)}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ucsb.cs.epsilon.ucsb360.FriendManager#getNumShares(java.lang.String)}.
+	 */
+	@Test
+	public void testGetNumShares() {
+		fail("Not yet implemented");
+	}
+
+}
