305a62658e696a606dc4cdfc0a9c8e6ba5e5a3a5
jfaghihnassiri <jfaghihnassiri@gmail.com>
2013-05-01 18:24:56 -0700

    Revert "Quick example for Jhon"
    This reverts commit 4cac4ea0459f8671be55ed9af5a03ee9f5d983c3.

--- jni/SampleUtils.cpp
+++ jni/SampleUtils.cpp
@@ -19,10 +19,6 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
-#include <vector>
-
-std::vector<int> myVec;
-
 void SampleUtils::printMatrix(const float* mat)
 {
     for (int r = 0; r < 4; r++, mat += 4)
