7e7f85728e64743f4ba26f1bf6b430cf75f6c0b8
Andy <Andy@169.231.86.165>
2013-05-07 18:15:38 -0700

    Method for taking augmentation screenshot completed. Method call yet will be checked in later.

--- src/com/qualcomm/QCARSamples/CloudRecognition/CloudRecoRenderer.java
+++ src/com/qualcomm/QCARSamples/CloudRecognition/CloudRecoRenderer.java
@@ -13,12 +13,24 @@
 
 package com.qualcomm.QCARSamples.CloudRecognition;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.IntBuffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.os.Environment;
 
 import com.qualcomm.QCAR.QCAR;
+import com.qualcomm.QCARSamples.CloudRecognition.utils.DebugLog;
 
 /** The renderer class for the CloudReco sample. */
 public class CloudRecoRenderer implements GLSurfaceView.Renderer
@@ -35,6 +47,15 @@ public class CloudRecoRenderer implements GLSurfaceView.Renderer
     public native void updateRendering(int width, int height);
 
 
+    //current view width and height
+    private int mViewWidth = 0;
+    private int mViewHeight = 0;
+    
+    //date
+    private Date date;
+    
+    //boolean for taking Screen shot
+    public boolean takeScreenShot = false;
     /** Called when the surface is created or recreated. */
     public void onSurfaceCreated(GL10 gl, EGLConfig config)
     {
@@ -54,7 +75,10 @@ public class CloudRecoRenderer implements GLSurfaceView.Renderer
         // parameters
         // have changed:
         updateRendering(width, height);
-
+        
+        mViewWidth = width;
+        mViewHeight = height;
+        
         // Call QCAR function to handle render surface size changes:
         QCAR.onSurfaceChanged(width, height);
     }
@@ -72,10 +96,70 @@ public class CloudRecoRenderer implements GLSurfaceView.Renderer
             return;
         }
 
+
         // Update render view (projection matrix and viewport) if needed:
         mActivity.updateRenderView();
 
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_ddHH_mm_ss");
+        
         // Call our native function to render content
         renderFrame();
+        // make sure the OpenGL rendering is finalized
+        GLES20.glFinish();
+     
+        if ( takeScreenShot ) {
+            saveScreenShot(0, 0, mViewWidth, mViewHeight, dateFormat.format(date)+".png");
+            takeScreenShot = false;
+        }
+    }
+    
+    private void saveScreenShot(int x, int y, int w, int h, String filename) {
+        Bitmap bmp = grabPixels(x, y, w, h);
+        try {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filename;
+            DebugLog.LOGD(path);
+             
+            File file = new File(path);
+            file.createNewFile();
+             
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(CompressFormat.PNG, 100, fos);
+  
+            fos.flush();
+             
+            fos.close();
+             
+        } catch (Exception e) {
+            DebugLog.LOGD(e.getStackTrace().toString());
+        }
+    }
+  
+    private Bitmap grabPixels(int x, int y, int w, int h) {
+        int b[] = new int[w * (y + h)];
+        int bt[] = new int[w * h];
+        IntBuffer ib = IntBuffer.wrap(b);
+        ib.position(0);
+         
+        GLES20.glReadPixels(x, 0, w, y + h, 
+                   GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, ib);
+  
+        for (int i = 0, k = 0; i < h; i++, k++) {
+            for (int j = 0; j < w; j++) {
+                int pix = b[i * w + j];
+                int pb = (pix >> 16) & 0xff;
+                int pr = (pix << 16) & 0x00ff0000;
+                int pix1 = (pix & 0xff00ff00) | pr | pb;
+                bt[(h - k - 1) * w + j] = pix1;
+            }
+        }
+  
+        Bitmap sb = Bitmap.createBitmap(bt, w, h, Bitmap.Config.ARGB_8888);
+        return sb;
+    }
+    
+    public void takeScreenShot()
+    {
+    	takeScreenShot = true;
+        date = new Date();
     }
 }
